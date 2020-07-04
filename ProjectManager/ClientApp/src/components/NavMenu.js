086f6a827ce11c0b23dcc7197efd5a26b223c65a
multicatch <cezaryre@gmail.com>
2020-07-04 17:37:40 +0200

    Added logout and removed unused classes

--- ProjectManager/ClientApp/src/components/NavMenu.js
+++ ProjectManager/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import {request} from "../requests";
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -31,13 +32,13 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Projects</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" onClick={this.logout}>Logout</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
@@ -45,5 +46,10 @@ export class NavMenu extends Component {
         </Navbar>
       </header>
     );
+  }
+  
+  logout = async () => {
+    await request('authentication', 'DELETE')
+    window.location.reload()
   }
 }
